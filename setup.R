b59aebd68e24e538f68f350320c5e8ed6807a014
Russell-Shean <russshean@gmail.com>
2026-05-27 21:32:05 +0800

    add package version step

--- setup.R
+++ setup.R
@@ -40,16 +40,16 @@
 # How these dependencies should be installed is probably best determined on
 # case-by-case basis depending on the user and the environment. To install them
 # locally on a personal laptop you can use the following:
-#     install.packages("devtools")
+#     install.packages(c("devtools", "roxygen2", "testthat", "knitr"))
 
 # Load packages ---------------
 # These packages are used for creating an R package and implementing features
 # such as testing and documentation
 
-# (An aside about my philosopy about namespaces in R: I like to load libraries normally
-#  using librar() AND refer to them by their::namespace() I think the slight performance
+# (An aside about my philosophy about namespaces in R: I like to load libraries normally
+#  using library() AND refer to them by their::namespace() I think the slight performance
 #  hit is worth the additional clarity and reduced risk of namespace conflict.
-# This is also the recomended method for package development)
+# This is also the recommended method for package development)
 
 # (Another aside: For similar reasons I load tidyverse and devtools packages
 # individual instead of by loading the metapackage
@@ -69,4 +69,17 @@ list.files(pattern = "\\.Rproj$") |>
 
 # create the package from one level up
 usethis::create_package("../PackagesDemo")
+
+
+# Add packages ----------------------------------------------------------------
+# Each project will obviosly have some unique dependencies, but it's probably
+# also safe to assume that some commonly used package versions can be standardized
+# e.g. dplyr. Any template may already come with pinned version of these common
+# packages, but for example here's how unique packages would be added:
+
+usethis::use_package("dplyr", min_version = packageVersion("dplyr") )
+
+
+
+
 
